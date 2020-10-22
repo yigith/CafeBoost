d24196e4ffe634740c64e49f1875f973e7cff610
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2020-10-22 10:50:24 +0300

    classes added

---
+++ CafeBoost.Data/SiparisDetay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CafeBoost.Data
+{
+    public class SiparisDetay
+    {
+        public string UrunAd { get; set; }
+        public decimal BirimFiyat { get; set; }
+        public int Adet { get; set; }
+
+        public string TutarTL { get { return $"{Tutar():0.00}TL"; } }
+
+        public decimal Tutar()
+        {
+            return Adet * BirimFiyat;
+        }
+    }
+}
---
+++ CafeBoost.Data/Urun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CafeBoost.Data
+{
+    public class Urun
+    {
+        public string UrunAd { get; set; }
+        public decimal BirimFiyat { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1:0.00}₺)", UrunAd, BirimFiyat); 
+        }
+    }
+}
